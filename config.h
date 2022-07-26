c9ca64aa2b7aaf7cf9cc28ab8a9dfc53ad8fe375
explosion-mental <explosion0mental@gmail.com>
2022-07-26 11:36:56 -0500

    config.h: comment how to find the governors on the system

--- config.h
+++ config.h
@@ -3,6 +3,9 @@ static const int mincpu      = 20;       /* percentage of cpu usage where turbo 
 static const int mintemp     = 70;       /* degrees celsius where turbo boost is enabled */
 static const int interval    = 5;        /* seconds to wait before running */
 static const int alwaysturbo = 0;        /* 1 means never disables turbo boost */
+
+/* available governors: cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors */
+
 /* default governor to use when using the battery */
 static const char batgovernor[] = "powersave";
 /* default governor to use when AC adapter is connected */
