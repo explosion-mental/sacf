5f23ccdf44ed7eb91873a71a59f700677de729a9
explosion-mental <explosion0mental@gmail.com>
2022-07-26 11:38:07 -0500

    config.mk: remove gnu extensions on debug flags

--- config.mk
+++ config.mk
@@ -11,8 +11,8 @@ MANPREFIX = ${PREFIX}/share/man
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
 CFLAGS   = -g -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -g
-#CFLAGS += -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-#LDFLAGS +=
+#CFLAGS  = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+#LDFLAGS =
 
 # compiler and linker
 CC = cc
