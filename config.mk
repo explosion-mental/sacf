728f9dc34524929ae4d7e2fa290632186e76ba31
explosion-mental <explosion0mental@gmail.com>
2022-08-08 21:23:18 -0500

    add better debug flags

--- config.mk
+++ config.mk
@@ -9,10 +9,10 @@ MANPREFIX = ${PREFIX}/share/man
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
-CFLAGS   = -g -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -g
-#CFLAGS  = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-#LDFLAGS =
+CFLAGS  = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+#CFLAGS  = -g -std=c99 -pedantic -Wall -Wextra -O3 ${INCS} ${CPPFLAGS} -flto -fsanitize=address,undefined,leak
+LDFLAGS =
+#LDFLAGS = -g ${CFLAGS}
 
 # compiler and linker
 CC = cc
