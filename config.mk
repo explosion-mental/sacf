d03143e739502771664396da9f3fcabed1744c11
explosion-mental <explosion0mental@gmail.com>
2022-08-22 19:03:53 -0500

    add git exact commit version

--- config.mk
+++ config.mk
@@ -1,6 +1,9 @@
 # sacf version
 VERSION = 1.5
 
+# sacf git exact commit version
+SRCVERSION = $$(git describe --tags --dirty=[modified])
+
 # Customize below to fit your system
 
 # paths
@@ -8,7 +11,7 @@ PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${SRCVERSION}\"
 CFLAGS  = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 #CFLAGS  = -g -std=c99 -pedantic -Wall -Wextra -O3 ${INCS} ${CPPFLAGS} -flto -fsanitize=address,undefined,leak
 LDFLAGS =
